ce358d945b67c9d1287aa2c22549ca8b48247cb2
Collard <AXC89YH>
2019-04-18 10:17:47 -0400

    adds new version of marquee to fix stutter

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import '../../styles/Footer/index.css'
+import Marquee from 'react-smooth-marquee'
 
 export default class Footer extends Component {
   
   render() {
     return (
       <div className="footer">
-        <p className="marquee">
+        <Marquee velocity='.3'>
           {this.props.text}
-        </p>
+        </Marquee>
       </div>
     )
   }
