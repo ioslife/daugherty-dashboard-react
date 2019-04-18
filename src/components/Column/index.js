9c2ab816b91db4554d5725ecb3dfa03d462e48b9
Collard <AXC89YH>
2019-04-18 10:38:15 -0400

    fixes columns

--- src/components/Column/index.js
+++ src/components/Column/index.js
@@ -4,13 +4,7 @@ import '../../styles/Column/index.css'
 export default class Column extends Component {
     render() {
       return (
-        <div className='welcomeCol'>
-            <div className="welcomeText">
-                Welcome to <b>Daugherty</b>!
-            </div>
-            
-            <div dangerouslySetInnerHTML={{ __html: this.props.column}} />
-        </div>
+          <div className='welcomeCol' dangerouslySetInnerHTML={{ __html: this.props.column}} />
       )
     }
 }
