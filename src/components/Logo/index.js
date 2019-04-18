7ebd6a5024137076203204f6971b76ca074d5ed4
Collard <AXC89YH>
2019-04-18 14:46:08 -0400

    fixes import for logo

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import image from '../../apple-touch-icon.png'
+import '../../styles/Logo/index.css'
 export default class Logo extends Component {
     render() {
       return (
-        <img className='appLogo' src={image} alt=''></img>
+        <img className='appLogo' src={image} alt='' />     
       )
     }
 }
