324646a9bc963df753f8bcd0c9202fa58a9baee4
Bronson Lane <bronsonglane@me.com>
2019-05-02 09:47:36 -0400

    updates marquee speed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,7 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="footer col-md-12">
-        <Marquee velocity='.3'>
+        <Marquee velocity='.1'>
           {this.props.text}
         </Marquee>
       </div>
