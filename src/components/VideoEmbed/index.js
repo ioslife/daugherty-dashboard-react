6d7e23703273b6b287d8314f4111f6b0eb2842a5
Bronson Lane <bronsonglane@me.com>
2019-06-24 17:35:34 -0400

    all

--- src/components/VideoEmbed/index.js
+++ src/components/VideoEmbed/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import '../../styles/VideoEmbed/index.css'
+import {fetchVideos} from '../../services/pullVideos'
 
 export default class VideoEmbed extends Component {
+
+  
+
     render() {
+      var func = () => {
+        var videos = fetchVideos()
+        console.log(videos)
+      }
       return (
         <div className="videoDiv col-md-8">
-          <iframe className="video-player" src={this.props.url + '/embed'} frameBorder="0" title={this.props.title} allow="autoplay; fullscreen" allowFullScreen>
-          </iframe>
+          {func()}
         </div>
       )
     }
