8691c08489b62a794dd90f19cdf97b3b3d137a6e
arthurCollard <collard_aj@yahoo.com>
2019-06-25 17:11:34 -0400

    nailed it... video auto plays. now need to loop through album

--- src/components/VideoEmbed/index.js
+++ src/components/VideoEmbed/index.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react'
 import '../../styles/VideoEmbed/index.css'
 import {fetchVideos} from '../../services/pullVideos'
+var $ = require('jquery')
+var HtmlToReactParser = require('html-to-react').Parser;
+
+var func = (callback) => {
+  fetchVideos(callback)
+}
 
 export default class VideoEmbed extends Component {
-
-  
-
-    render() {
-      var func = () => {
-        var videos = fetchVideos()
-        console.log(videos)
+    constructor(props){
+      super(props)
+      this.state = {
+        url: ''
       }
+    }
+    componentWillMount() {
+      func((data) => {
+        this.setState({url:data})
+          console.log()
+      })
+    }
+    videoController(){
+      var htmlToReactParser = new HtmlToReactParser();
+      if(this.state.url === ""){
+        return <div></div>
+      } else {
+        
+        var player = $(this.state.url.data[0].embed.html)
+        player.addClass('video-player')
+        player.width = null
+        player.height = null
+        player[0].src += "&autoplay=1&muted=1"
+        console.log(player)
+        console.log(player[0])
+        console.log(player[0].outerHTML)
+        console.log(this.state.url.data[0].embed.html)
+        return(htmlToReactParser.parse(player[0].outerHTML))
+      }
+    }
+    render() {
+      
       return (
         <div className="videoDiv col-md-8">
-          {func()}
+          {/* <iframe className="video-player" src={this.props.url + '/embed'} frameBorder="0" title={this.props.title} allow="autoplay; fullscreen" allowFullScreen>
+          </iframe> */}
+          {this.videoController()}
         </div>
       )
     }
--- src/services/pullVideos.js
+++ src/services/pullVideos.js
@@ -1,13 +1,13 @@
-
 let Vimeo = require("vimeo").Vimeo;
 
-export const fetchVideos = () => {
+export function fetchVideos (callback) {
   let client = new Vimeo(
     "e956e972ca390e25943fc44472e9298635a4482b",
     "Yf7h1SmMUx9yGiNjBSv4xD9DS946xsnFSMTFZTQk9sXCJE9i+2R/GFg3UbHAS06MXfEhnMDZJ6heTBAUbf/4a4zLCaAky4oKMjW2bpn0uLMIimMhgak6n55aFjk8LQNg",
     "9d00b7af5e978de6b77bd813e1793c6a"
   );
-   return client.request(
+  
+  client.request(
     {
       method: "GET",
       path: "/me/albums/5828482/videos"
@@ -16,9 +16,7 @@ export const fetchVideos = () => {
       if (error) {
         console.log(error);
       }
-      return new Promise((resolve, reject) => {
-        resolve(body)
-      })
+      callback(body)
     }
-  );
+  )
 };
