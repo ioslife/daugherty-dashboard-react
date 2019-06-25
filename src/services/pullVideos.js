6deedafc5f18bed41b712ee464bd899538356d26
Bronson Lane <bronsonglane@me.com>
2019-06-25 15:14:29 -0400

    add

---
+++ src/services/pullVideos.js
@@ -0,0 +1,24 @@
+
+let Vimeo = require("vimeo").Vimeo;
+
+export const fetchVideos = () => {
+  let client = new Vimeo(
+    "e956e972ca390e25943fc44472e9298635a4482b",
+    "Yf7h1SmMUx9yGiNjBSv4xD9DS946xsnFSMTFZTQk9sXCJE9i+2R/GFg3UbHAS06MXfEhnMDZJ6heTBAUbf/4a4zLCaAky4oKMjW2bpn0uLMIimMhgak6n55aFjk8LQNg",
+    "9d00b7af5e978de6b77bd813e1793c6a"
+  );
+   return client.request(
+    {
+      method: "GET",
+      path: "/me/albums/5828482/videos"
+    },
+    (error, body, status_code, headers) => {
+      if (error) {
+        console.log(error);
+      }
+      return new Promise((resolve, reject) => {
+        resolve(body)
+      })
+    }
+  );
+};
